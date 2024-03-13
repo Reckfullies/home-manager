4a7ddb650b006b97f726e2529bf5f515cf6b341f
bricked <git@bricked.dev>
2024-03-13 22:43:24 +0100

    firefox: add floorp browser

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -941,6 +941,11 @@ in {
       unwrappedPackage = "firefox-unwrapped";
       visible = true;
     };
+    floorp = browserOptions {
+      name = "Floorp";
+      package = "floorp";
+      unwrappedPackage = "floorp-unwrapped";
+    };
   };
 
   config = mkMerge [
@@ -954,6 +959,15 @@ in {
         "Library/Application Support/Firefox"
       else
         "${vendorPath}/firefox";
+      isWrapped = true;
+    })
+    (browserConfig {
+      name = "Floorp";
+      module = "floorp";
+      browserPath = if isDarwin then
+        "Library/Application Support/Floorp"
+      else
+        ".floorp";
       isWrapped = true;
     })
   ] // {
